a65b0973914668adc98636e8aad4a300d867a20e
Dave Disser <disser@gmail.com>
2015-02-28 13:34:45 -0800

    Clean up the C version.

--- fast/who_wins.c
+++ fast/who_wins.c
@@ -3,11 +3,14 @@
 #include <stdint.h>
 #include <time.h>
 #include <strings.h>
+#include "xorshift.h"
 
 #define PLAYER1_THRESHOLD 65
-#define PLAYER2_THRESHOLD 60
-#define ITERATIONS 100000000
-#define spin() (5*(1+random()%20))
+#define PLAYER2_THRESHOLD 55
+#define ITERATIONS 1000000
+#define spin() (5*(1+xor32()%20))
+
+extern char *optarg;
 
 char *result_text[] = {
 	"NaN",
@@ -54,15 +57,38 @@ uint8_t who_wins(uint8_t player1_threshold, uint8_t player2_threshold)
 
 uint8_t main(int argc, char *argv[])
 {
-	srandom(time(NULL));
 	uint32_t results[8];
 	uint32_t i;
 
-	bzero(results, sizeof(results));
+	uint8_t player1_threshold = PLAYER1_THRESHOLD;
+	uint8_t player2_threshold = PLAYER2_THRESHOLD;
+	int iterations = ITERATIONS;
+	int opt;
 
-	for (i = 0; i < ITERATIONS; ++i)
-		results[who_wins(PLAYER1_THRESHOLD, PLAYER2_THRESHOLD)]++;
+	while ((opt = getopt(argc, argv, "1:2:n:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'n':
+				iterations = atoi(optarg);
+				break;
+			case '1':
+				player1_threshold = atoi(optarg);
+				break;
+			case '2':
+				player2_threshold = atoi(optarg);
+				break;
+			default:
+				fprintf(stderr, "Usage: %s [-n iterations] [-1 player1_threshold] [-2 player2_threshold]\n",
+						argv[0]);
+				exit(1);
+		}
+	}
+
+	bzero(results, sizeof(results));
+	for (i = 0; i < iterations; ++i)
+		results[who_wins(player1_threshold, player2_threshold)]++;
 
 	for (i = 1; i < 8; i++)
-		printf("%s: %3.2f\n", result_text[i], 100.0 * results[i] / ITERATIONS);
+		printf("%s: %3.2f\n", result_text[i], 100.0 * results[i] / iterations);
 }
